d5d73a46d86f75f68f04b7f8502e4ce16ed286c7
Natalia <sh.nat.vlad@gmail.com>
2024-06-12 10:41:46 +0500

    test: add test to not render some text

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -15,13 +15,12 @@ describe('Greeting component', () => {
     expect(helloWorldElement).toBeInTheDocument();
   });
 
-  test('renders "good to see" you if the button was NOT clicked', () => {
+  test('renders "good to see you" if the button was NOT clicked', () => {
     render(<Greeting />);
     const outputElement = screen.getByText('good to see you', { exact: false });
     expect(outputElement).toBeInTheDocument();
   });
   test('renders "Changed!" if the button was clicked', () => {
-    
     // Arrange
     render(<Greeting />);
 
@@ -32,6 +31,18 @@ describe('Greeting component', () => {
     // Assert
     const outputElement = screen.getByText('Changed!');
     expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+
+    // Assert
+    const outputElement = screen.queryByText('good to see you', { exact: false });
+    expect(outputElement).toBeNull();
   });
 })
 
